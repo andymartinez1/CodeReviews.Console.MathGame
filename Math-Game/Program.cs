88594ed943bd066f9de2318f2cb55e59930b0834
Andrew Martinez <andy72391@gmail.com>
2025-05-22 15:51:03 -0400

    Added functionality to all games (Addition, Subtraction, Multiplication, Division) and added a list to keep track of previous game scores. Working on refactoring using OOP principles.

--- Math-Game/Program.cs
+++ Math-Game/Program.cs
@@ -1,62 +1,17 @@
-﻿Console.WriteLine("Please type your name:");
+﻿using Math_Game;
 
-var name = Console.ReadLine();
+var menu = new Menu();
 var date = DateTime.Now;
 
-void Menu(string? name, DateTime date)
+var gamesPlayed = new List<string>();
+
+var name = GetName();
+
+menu.ShowMenu(name, date);
+
+string? GetName()
 {
-    Console.WriteLine("----------------------------------------------------");
-    Console.WriteLine($"Hello {name}! It's {date.DayOfWeek}.");
-    Console.WriteLine(@"What game would you like to play? Choose from the options below:
-A - Addition
-S - Subtraction
-M - Multiplication
-D - Division
-E - Exit");
-    Console.WriteLine("----------------------------------------------------");
-
-    var gameSelected = Console.ReadLine();
-
-    switch (gameSelected.Trim().ToLower())
-    {
-        case "a":
-            AdditionGame();
-            break;
-        case "s":
-            SubtractionGame();
-            break;
-        case "m":
-            MultiplicationGame();
-            break;
-        case "d":
-            DivisionGame();
-            break;
-        case "e":
-            Console.WriteLine("Goodbye");
-            Environment.Exit(0);
-            break;
-        default:
-            Console.WriteLine("You did not enter a valid option");
-            break;
-    }
-}
-
-void AdditionGame()
-{
-    Console.WriteLine("Addition Game selected");
-}
-
-void SubtractionGame()
-{
-    Console.WriteLine("Subtraction Game selected");
-}
-
-void MultiplicationGame()
-{
-    Console.WriteLine("Multiplication Game selected");
-}
-
-void DivisionGame()
-{
-    Console.WriteLine("Division Game selected");
+    Console.WriteLine("Please type your name:");
+    var name = Console.ReadLine();
+    return name;
 }
